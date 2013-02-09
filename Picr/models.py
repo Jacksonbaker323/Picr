29c7daf2f1a1c71ae9f14318fabfae3f3643d3b2
Jackson Baker <Jacksonbaker323@gmail.com>
2013-02-08 21:45:28 -0600

    Formatting changes and comments

--- Picr/models.py
+++ Picr/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 class Image(models.Model):
+	#Filename of the image (UUID4)
 	image_filename = models.CharField(max_length=1000)
+	#User submitted tags
 	image_tags = models.CharField(max_length=1000)
+	#Date that the image was uploaded
 	image_upload_date = models.DateTimeField()
 	def __unicode__(self):
 		return self.image_filename
--- Picr/views.py
+++ Picr/views.py
@@ -1,17 +1,22 @@
-# Create your views here.
+# Imports
 
 from django.shortcuts import render_to_response, render
 import time, uuid, os, datetime
-
 from Picr.models import Image
 from Picr.custom_objects import image_obj
-
 import urllib2
 
+
+
+#Main page
 def home(request):
 	return render_to_response('Picr/index.html')
 
+
+#Submitted image page
 def submitted(request):
+	
+	#Get the information from the submission
 	if request.GET['image'] == '':
 		return render_to_response('Picr/error.html')
 	else:
@@ -22,57 +27,47 @@ def submitted(request):
 	else:
 		tags = request.GET['tags']
 
-	
-	 
-
 #Save the image to a folder
-	
 	#Get the file extension
 	filename_extension = image.split('.')[-1]
 	#Give the file a UUID for a filename. In theory there should not be a name collision. 
 	local_filename = str(uuid.uuid4())
-
 	#Download the image itself
 	remote_file = urllib2.urlopen(image)
 	#Open up a local file
-	#!!!!Need to figure out how to save to a static directory!!!!!
 	local_file = open(str(os.getcwd()) + "/Picr/Images/" + local_filename + "." + filename_extension, "w" )
 	#Write the image to the local file
 	local_file.write(remote_file.read())
 	#Close out the file
 	local_file.close()
-
 #Save the tags, filename, and upload time to the database
-	
 	database_entry = Image(image_filename=local_filename + "." + filename_extension,image_tags=tags,image_upload_date=datetime.datetime.now())
 	database_entry.save()
 
-
-
+#Get the data for the page
 	context = {'image' : image, 'tags' : tags, 'local_filename': local_filename, 'filename_extension' : filename_extension }
 	return render_to_response('Picr/submitted.html', context)
 
-
+#Gallery page
 def gallery(request):
-
-# Write a custom class to hold the image data
-
-
+	
+	#Create a dictionary to hold the gallery images
 	gallery_dict = []
-
+	#List all of the images
 	list = Image.objects.all()
 
+	#Iterate through the list, get the image information, make an object with it and then append it to a dictionary.
 	for x in list:
 		item = image_obj(x.image_filename, x.image_tags, x.image_upload_date)
 		gallery_dict.append(item)
 
-
+#Get the data for the page
 	context = {'gallery_dict' : gallery_dict}
 	return render_to_response('Picr/gallery.html', context)
 
-
+#Search results page
 def search(request):
-	
+	#Get the information from the page
 	if request.GET['search'] == '':
 		context = {'error' : "No search tags submitted"}
 		return render_to_response('Picr/search.html', context)
@@ -80,20 +75,24 @@ def search(request):
 		tags = request.GET['search']
 		tags = tags.split(" ")
 		search_results = []
+		#Get the individual tag in the list of tags that the user submitted
 		for tag in tags:
+			#Check to see if any image contains that tags
 			list = Image.objects.filter(image_tags__contains=tag)
 			for item in list:
+				#Check to see if that image is already in the list
 				x = image_obj(item.image_filename, item.image_tags, item.image_upload_date)
+				#If it is in the list tlthen leave it out
 				if search_results.count(x) >= 1:
 					break
+				#Otherwise add it to the search_results list
 				else:
 					search_results.append(x)
 		
 		
 		
-		
+#Get the data for the page
 		context = {'tags' : tags, 'search_results' : search_results}
-	
 		return render_to_response('Picr/search.html', context)
 
 
